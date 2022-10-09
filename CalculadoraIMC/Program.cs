dd2d278aca4b62ad0954822da4617790ad0b3781
DavGuett <soulsec@protonmail.com>
2022-10-09 19:07:23 -0300

    Add project files.

---
+++ CalculadoraIMC/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Bem vindo à Calculadora de IMC");
+Console.WriteLine("Qual a sua altura?");
+Console.WriteLine("Qual o seu peso?");
+Console.WriteLine("O seu IMC se encontra entre: ");
