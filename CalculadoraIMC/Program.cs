efc45b4bd78836485316131f7ce30eb743780780
DavGuett <soulsec@protonmail.com>
2022-10-09 19:18:06 -0300

    Testado o funcionamento e o número de dígitos a ser mostrado no console

--- CalculadoraIMC/Program.cs
+++ CalculadoraIMC/Program.cs
@@ -1,9 +1,13 @@
 ﻿Console.WriteLine("Bem vindo à Calculadora de IMC");
 Console.WriteLine("Qual a sua altura?");
+double altura = double.Parse(Console.ReadLine());
 Console.WriteLine("Qual o seu peso?");
+double peso = double.Parse(Console.ReadLine());
+double imc = CalcularIMC(peso, altura);
 Console.WriteLine("O seu IMC se encontra entre: ");
+Console.WriteLine(Math.Round(imc, 2));
 
-double imc;
+
 
 static double CalcularIMC(double peso, double altura)
 {
